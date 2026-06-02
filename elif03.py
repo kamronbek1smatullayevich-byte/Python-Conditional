687aba1e9b5285899627e33c5055e56e2072088e
kamronbek1smatullayevich-byte <kamronbek1smatullayevich@gmail.com>
2026-06-03 01:29:06 +0500

    task03 done

--- elif03.py
+++ elif03.py
@@ -1,1 +1,11 @@
+son = int(input('son kiriting: '))
 
+if son % 2 == 0:
+    print("2 ga bo'linadi")
+
+if son % 3 == 0:
+    print("3 ga bo'linadi") 
+
+if son % 5 == 0:
+    print("5 ga bo'linadi") 
+     
