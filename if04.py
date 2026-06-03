bcdd0007e0c46fb24b17509ea94ccb67189eb030
kamronbek1smatullayevich-byte <kamronbek1smatullayevich@gmail.com>
2026-06-03 12:32:52 +0500

    task09 done

--- if04.py
+++ if04.py
@@ -1,1 +1,11 @@
+balance = 5000
 
+summa = int(input("Yechmoqchi bo'lgan summani kiriting: "))
+
+if summa < 0:
+    print("Manfiy summa kiritib bo'lmaydi.")
+elif summa <= balance:
+    balance = balance - summa
+    print("Pul yechildi. Qolgan balans:", balance, "so'm")
+else:
+    print("Mablag' yetarli emas. Sizning balansingiz:", balance, "so'm")
