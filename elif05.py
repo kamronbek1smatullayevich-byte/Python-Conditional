fd63231b259ca06bd66065c67b1d69f563b339b7
kamronbek1smatullayevich-byte <kamronbek1smatullayevich@gmail.com>
2026-06-03 12:14:36 +0500

    task05 done

--- elif05.py
+++ elif05.py
@@ -1,1 +1,11 @@
+temperature = int(input("havo harorati: "))
+
+if temperature < 0:
+    print("juda sovuq! Issiq kiyim kiying")
+
+if temperature >= 0 and temperature <= 14:
+    print("Sovuq. Kurtka kiying.")
+
+if temperature > 15:
+    print("Ob-havo yaxshi.")
 
