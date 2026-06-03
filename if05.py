9d72adcdef9645db06118f7a0417ad8e324e387b
kamronbek1smatullayevich-byte <kamronbek1smatullayevich@gmail.com>
2026-06-03 12:35:06 +0500

    task10 done

--- if05.py
+++ if05.py
@@ -1,1 +1,11 @@
+email = input("Email kiriting: ")
 
+if "@" in email and (
+    ".com" in email or
+    ".uz" in email or
+    ".net" in email or
+    ".org" in email
+):
+    print("Email qabul qilindi.")
+else:
+    print("Email noto'g'ri formatda.")
