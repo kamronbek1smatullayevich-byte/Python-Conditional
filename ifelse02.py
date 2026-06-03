8e2ada2a77000c64bf25f90157d72df49ff00205
kamronbek1smatullayevich-byte <kamronbek1smatullayevich@gmail.com>
2026-06-03 12:37:10 +0500

    task12 done

--- ifelse02.py
+++ ifelse02.py
@@ -1,1 +1,17 @@
+a = float(input("1-sonni kiriting: "))
+b = float(input("2-sonni kiriting: "))
+amal = input("Amalni kiriting (+, -, *, /): ")
 
+if amal == "+":
+    print(a + b)
+elif amal == "-":
+    print(a - b)
+elif amal == "*":
+    print(a * b)
+elif amal == "/":
+    if b == 0:
+        print("Nolga bo'lish mumkin emas!")
+    else:
+        print(a / b)
+else:
+    print("Noto'g'ri amal. Faqat +, -, *, / ishlatiladi.")
