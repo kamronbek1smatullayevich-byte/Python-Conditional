521dd097fb301b2709bbb5d9ac6c55e7d5deca48
kamronbek1smatullayevich-byte <kamronbek1smatullayevich@gmail.com>
2026-06-03 12:08:35 +0500

    task04 done

--- elif04.py
+++ elif04.py
@@ -1,1 +1,12 @@
-
+age = int(input('yosh: '))
+price = 100
+if age < 7:
+    chegirma = 50
+    print(price - chegirma, "50 foiz chegirma qo'llanildi")
+if age > 7 and age < 17:
+    chegirma = 20
+    print(price - chegirma, "20 foiz cehgirma qo'llanildi")
+if age > 60:
+    chegirma = 30
+    print(price - chegirma, "30 foiz cehgirma qo'llanildi") 
+           
