6e6eb10c94c1113c6c3b1998086366434465e5ba
kamronbek1smatullayevich-byte <kamronbek1smatullayevich@gmail.com>
2026-06-03 12:36:20 +0500

    task11 done

--- ifelse01.py
+++ ifelse01.py
@@ -1,1 +1,14 @@
+ball = int(input("Ball kiriting: "))
 
+if ball < 0 or ball > 100:
+    print("Ball 0-100 oralig'ida bo'lishi kerak!")
+elif ball >= 90:
+    print("A (A'lo)")
+elif ball >= 80:
+    print("B (Yaxshi)")
+elif ball >= 70:
+    print("C (Qoniqarli)")
+elif ball >= 60:
+    print("D (Qoniqarsiz)")
+else:
+    print("F (Rad)")
