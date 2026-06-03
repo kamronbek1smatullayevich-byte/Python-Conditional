a8209b0ad3433867252237c04a90864b32da9587
kamronbek1smatullayevich-byte <kamronbek1smatullayevich@gmail.com>
2026-06-03 12:38:03 +0500

    task13 done

--- ifelse03.py
+++ ifelse03.py
@@ -1,1 +1,12 @@
+soat = int(input("Soatni kiriting: "))
 
+if soat < 0 or soat > 23:
+    print("Soat 0-23 oralig'ida bo'lishi kerak!")
+elif soat >= 5 and soat <= 11:
+    print("Ertalab")
+elif soat >= 12 and soat <= 17:
+    print("Kunduzi")
+elif soat >= 18 and soat <= 21:
+    print("Kechqurun")
+else:
+    print("Tun")
